7589aaa4781faef248a5449893f00fcec0b6e6fc
FranciscoAnjos <fcariaanjos@gmail.com>
2024-11-08 13:20:09 +0000

    Did exercise for week1

---
+++ character.py
@@ -0,0 +1,14 @@
+﻿creatures = []
+
+for i in range(3):
+    cname = input()
+    chp = int(input())
+    clvl = int(input())
+    creatures.append([cname, (chp, clvl)]) #store HP and level as tuple
+
+print(creatures)
+
+creatures.sort(key=lambda tup: tup[1][1], reverse=True) #sort by level, decreasing
+
+for i in range(len(creatures)):
+    print(creatures[i][0]) #print just name
